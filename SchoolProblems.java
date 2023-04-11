9d92ec6c85ef9527a4fe5b1079ef18006a7aac4a
NCRaider1 <92733178+NCRaider1@users.noreply.github.com>
2023-04-11 00:17:47 -0400

    the array was implemented

--- SchoolProblems.java
+++ SchoolProblems.java
@@ -1,6 +1,7 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class SchoolProblems
+public class SchoolProblems 
 {
     // initialises gradYear int
     private int gradYear;
@@ -23,42 +24,55 @@ public class SchoolProblems
         this.gradYear = newGradYear;
     }
 
-    public static void main (String[] args)
+    public static void main(String[] args) 
     {
-
+        int[] spots = new int[25];
+        Random rnd = new Random();
+        int numRemoved = 0;
 
         // welcomes user to QU parking lot
         System.out.println("Hello and welcome to the Quinnipiac parking system. Are you a student or are you faculty?.");
         String studentOrFaculty = input.nextLine();
         // Checks for special circumstances
-        if (studentOrFaculty.equals("faculty"))
+        if (studentOrFaculty.equals("faculty")) 
         {
             // checks for president judy and if not welcomes regular faculty
             System.out.println("Are you by any chance Judy Olian?");
             String judy = input.nextLine();
-            if (judy.equals("yes"))
-            {
+            if (judy.equals("yes")) {
                 System.out.println("Welcome Mrs president your special spot number 1 is available");
-            }
-            else
-            {
+                spots[0] = 1;
+            } else {
                 System.out.println("Welcome faculty");
+                while (true) {
+                    int spot = rnd.nextInt(25) + 1;
+                    if (spots[spot - 1] == 0) 
+                    {
+                        spots[spot - 1] = 1;
+                        System.out.println("Your assigned spot is: " + spot);
+                        break;
+                    }
+                }
             }
-        } 
-        // if student, it checks for grad year and if permitted to park
-        else
-        {
+        } else {
             System.out.println("Your expected year of graduation?");
             SchoolProblems schoolProblems = new SchoolProblems();
             schoolProblems.setGradYear(input.nextInt());
 
-            if (schoolProblems.getGradYear() == 2026)
-            {
+            if (schoolProblems.getGradYear() >= 2026) {
                 System.out.println("Sorry, freshman are not allowed to park on campus.");
-            }
-            else
-            {
+            } else {
                 System.out.println("Welcome to the wonderful QU parking lot");
+                while (true) 
+                {
+                    int spot = rnd.nextInt(25) + 1;
+                    if (spots[spot - 1] == 0) 
+                    {
+                        spots[spot - 1] = 1;
+                        System.out.println("Your assigned spot is: " + spot);
+                        break;
+                    }
+                }
             }
         }
     }
